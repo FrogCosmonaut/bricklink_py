86f57e1f5406b84e982bba6f33c4700a6316a3c2
FrogCosmonaut <git@juanfg.es>
2025-03-07 17:32:55 +0100

    fix: package version

--- bricklink_py/__init__.py
+++ bricklink_py/__init__.py
@@ -3,7 +3,7 @@ from .bricklink import Bricklink
 __all__ = ["Bricklink"]
 
 # Package metadata
-__version__ = "0.1.0-beta"
+__version__ = "0.1.1-beta"
 __author__ = "Juan Franco"
 __email__ = "bricklink_py@juanfg.es"
 __description__ = "A wrapper for interacting with the Bricklink API."
